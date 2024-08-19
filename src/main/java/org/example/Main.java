b2dca7cbe2d45f4f04c91851d4ab7ec57e3ea30d
Nathan Perez <naperezmail@gmail.com>
2024-08-19 16:01:43 -0400

    added javadoc comments for Fibonacci method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,6 +12,12 @@ public class Main {
     }
 
 
+    /**
+     * Finds the nth number of the Fibonacci Sequence using recursion
+     *
+     * @param n the place of the element to find
+     * @return the number at the nth place
+     */
     public static int Fibonacci(int n) {
 
         // first & second element of fib are always 0 & 1
